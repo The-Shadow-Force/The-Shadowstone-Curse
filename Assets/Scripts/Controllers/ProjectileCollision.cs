17420cdd40f230265ad098decc8ac1133f60b2ec
quanghoaluu1 <hoalqse183138@fpt.edu.vn>
2025-07-16 10:07:03 +0700

    fix

--- Assets/Scripts/Controllers/ProjectileCollision.cs
+++ Assets/Scripts/Controllers/ProjectileCollision.cs
@@ -12,7 +12,7 @@ public class ProjectileCollision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             onHit?.Invoke();
         }
